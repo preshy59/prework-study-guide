70018b69f8d66e224c77f7ba30b8363f5efda28a
Ebele Okemba <okemba50@gmail.com>
2022-11-19 19:22:12 +0000

    Added basic JavaScript

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var topic = ["HTML", "CSS", "Git", "JavaScript"];
+for (var names = 0; names < topic.length; names++) {
+    console.log(topic[names]);
+    
+}
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
